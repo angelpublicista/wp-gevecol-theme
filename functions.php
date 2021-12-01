f626b0c3c6d94007711fa83c0f8259cb90592126
Angeldevelop <you@example.com>
2021-12-01 11:40:32 -0500

    Slick min css

--- functions.php
+++ functions.php
@@ -22,6 +22,20 @@ function hello_elementor_child_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts', 20 );
 
+
+function gev_slick_assets_enqueue(){
+	// Slick js
+	wp_enqueue_script( 'gev-slick-js', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js', array('jquery'), '1.8.1');
+
+	// Slick css
+	wp_enqueue_style( 'gev-slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css');
+
+	// Slick theme css
+	wp_enqueue_style( 'gev-slick-theme-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'gev_slick_assets_enqueue');
+
 // Custom post types
 require('inc/cpt/cpt-graphics.php');
 
